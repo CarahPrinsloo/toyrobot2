97ba074835b2befb7ddfbebe48c424080e6cf4ed
CarahPrinsloo <49373177+CarahPrinsloo@users.noreply.github.com>
2020-09-01 07:01:58 +0200

    Update toyrobot2.py

--- toyrobot2.py
+++ toyrobot2.py
@@ -1,7 +1,24 @@
+def name_robot():
+    name = input('What do you want to name your robot? ')
+    while name == '':
+        name = input('What do you want to name your robot? ')
+    print(name, end = '')
+    print(': Hello kiddo!')
+    return name
+
+def choose_command(command):
+    lowercase_command = command.lower().strip()
+    if lowercase_command == 'off':
+        return 'off'
+
+
+def turn_off_robot():
+    print('Shutting down..')
+
+
 def robot_start():
     """This is the entry function, do not change"""
-    pass
-
-
-if __name__ == "__main__":
-    robot_start()
+    name = name_robot()
+    off = False
+    while not off:
+        
